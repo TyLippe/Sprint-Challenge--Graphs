2aaf67e2b92994649313414dc39ef248bae9f67a
Ty Lippe <tylippe94@gmail.com>
2020-02-20 16:36:56 -0500

    ability to check exits, add to visited, add to traversal

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from stack import Stack
 
 import random
 from ast import literal_eval
@@ -12,9 +13,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -48,15 +49,51 @@ else:
 
 
 
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# #######
+# # UNCOMMENT TO WALK AROUND
+# #######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
+
+
+def dft(self, starting_vertex, traversal_path):
+        # Create an empty Stack
+        s = Stack()
+        # Add A PATH TO the starting_vertex to the queue
+        s.push(starting_vertex)
+        # Create an empty set to store visited rooms
+        visited_rooms = set()
+        # While the queue is not empty...
+        while s.size() > 0:
+            exits = list()
+            # Get all avaible exits of current room
+            for door in player.current_room.get_exits():
+                exits += list(door)
+            print(f'Exits: {exits}')
+            # Dequeue, the first PATH
+            v = s.pop()
+            # Check if it has been visited
+            # If not visited...
+            if v not in visited_rooms:
+                # Add current room to visited_rooms
+                visited_rooms.add(v)
+                # Move player and add new room to visited
+                player.travel('n')
+                # visited_rooms.add(player.current_room.id)
+                s.push(player.current_room.id)
+                print(f'Visited: {visited_rooms}')
+                if 'n' in exits:
+                    traversal_path.append('n')
+        print(traversal_path)
+                
+
+
+dft(player, 0, traversal_path)
+print(len(traversal_path))
